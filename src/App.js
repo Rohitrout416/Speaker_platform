5485687976cab1cbb096b8dc507edc5967b2a8c0
Rohitrout416 <79717560+Rohitrout416@users.noreply.github.com>
2023-06-08 13:26:09 +0530

    Added the basic web template for speaker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+import Heading from './components/Heading';
 import './App.css';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <Tabs align='end' isFitted variant='enclosed' className="tabs-container">
+        <TabList className="tab-list">
+          <Heading className="heading" />
+          <Tab className="tab">About</Tab>
+          <Tab className="tab">Courses</Tab>
+          <Tab className="tab">Galleries</Tab>
+          <Tab className="tab">Testimonies</Tab>
+          <Tab className="tab">Contact</Tab>
+        </TabList>
+        <TabPanels className="tab-panels">
+          <TabPanel className="tab-panel">
+            <About />
+          </TabPanel>
+          <TabPanel className="tab-panel">
+            <p>two!</p>
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
     </div>
   );
 }
---
+++ src/components/Heading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Heading(){
+    return(
+        <div className="heading">
+            Amar Bagh
+        </div>
+    )
+}
+
+export default Heading;
